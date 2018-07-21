0bdf3aef48fcd194320b4591c47c6806c11bd46d
Antonio Velázquez <38739674+antoniovs1029@users.noreply.github.com>
2018-07-21 03:53:08 -0500

    Añadidos plots para las distribuciones tópico-palabra

--- topics_viz/plots.py
+++ topics_viz/plots.py
@@ -1,9 +1,13 @@
 from topics_viz import app, db
 from topics_viz.models import *
+
 from bokeh.plotting import figure
 from bokeh.embed import components
 from bokeh.models import ColumnDataSource
+from bokeh.io import show
+
 import numpy as np
+from sqlalchemy import func
 
 def topicid_nwords_vbar(ts_id):
     """
@@ -54,14 +58,12 @@ def topics_nwords_histogram(ts_id):
     nwords = np.array([x[0] for x in q2])
     hist, edges = np.histogram(nwords, range = (0, nwords.max()), bins = nwords.max())
     data = {'hist': hist, 'left': edges[:-1], 'right': edges[1:]}
-    interval = ['De {} a {} palabras'.format(x, y) for x, y in zip(data['left'], data['right'])]
-    data['interval'] = interval
 
     p = figure(plot_height = 600, plot_width = 600,
         x_axis_label = '# Palabras',
         y_axis_label = '# Topicos',
         tools="xpan, xwheel_zoom, box_zoom, reset, save",
-        tooltips=[("# De Topicos:", "@hist"), ("Intervalo", "@interval")]
+        tooltips=[("# De Topicos:", "@hist"), ("# Palabras", "@left")]
         )
 
     p.quad(
@@ -70,3 +72,154 @@ def topics_nwords_histogram(ts_id):
         )
 
     return p
+
+def wordid_ntopics_vbar(ts_id):
+    """
+    Diagrama de barras verticales (vbar) que tiene en el eje 'x' el ID de
+    las palabras, y en el eje 'y' el numero de topicos a los que pertenece en un
+    topicset.
+    """
+    q = db.session.query(WordTopicsNumber.ntopics).filter(WordTopicsNumber.topicset_id == ts_id)
+    word_id = [str(x) for x in range(q.count())]
+    ntopics = [x[0] for x in q]
+
+    data = {'word_id': word_id, 'ntopics': ntopics}
+
+    p = figure(
+        x_range= word_id,
+        plot_height=250,
+        x_axis_label = "ID de la Palabra",
+        y_axis_label ="# Topicos",
+        tools="xpan, xwheel_zoom, box_zoom, reset, save",
+        tooltips=[("ID", "@word_id"), ("# Topicos", "@ntopics")]
+        )
+
+    p.vbar(
+        source = data,
+        x = 'word_id',
+        top= 'ntopics',
+        width=0.9
+        )
+
+    p.xgrid.grid_line_color = None
+    p.y_range.start = 0
+
+    # Para borrar los ticks del eje x:
+    p.xaxis.major_label_text_font_size = "0pt"
+    p.xaxis.major_tick_in = 0
+    p.xaxis.major_tick_out = 0
+    return p
+
+def words_ntopics_histogram(ts_id):
+    """
+    Histograma del numero de tópicos en los que aparece una palabra.
+    El tamaño de las bins y del histograma se ajusta automaticamente, para que
+    cada bin represente un intervalo de un tópico.
+    """
+    q = db.session.query(WordTopicsNumber.ntopics).filter(WordTopicsNumber.topicset_id == ts_id)
+    ntopics = np.array([x[0] for x in q])
+    hist, edges = np.histogram(ntopics, range = (0, ntopics.max()), bins = ntopics.max())
+    data = {'hist': hist, 'left': edges[:-1], 'right': edges[1:]}
+    interval = ['De {} a {} tópicos'.format(x, y) for x, y in zip(data['left'], data['right'])]
+    data['interval'] = interval
+
+    p = figure(plot_height = 600, plot_width = 600,
+        x_axis_label = '# Tópicos',
+        y_axis_label = '# Palabras',
+        tools="xpan, xwheel_zoom, box_zoom, reset, save",
+        tooltips=[("# De Palabras:", "@hist"), ("# Topicos", "@left")]
+        )
+
+    p.quad(
+        source = data,
+        bottom=0, top='hist', left='left', right='right'
+        )
+
+    return p
+
+def twdis_summary(ts_id, twdis_id):
+    """
+    """
+    ids = []
+    maxes = []
+    mins = []
+    avgs = []
+    for topic in Topic.query.filter_by(topicset_id = ts_id):
+        ids.append(str(topic.id))
+        tmax, tmin, tavg = db.session.query(
+            func.max(TopicWordValue.value),
+            func.min(TopicWordValue.value),
+            func.avg(TopicWordValue.value)
+            )\
+            .filter(TopicWordValue.topicset_id == ts_id)\
+            .filter(TopicWordValue.twdis_id == twdis_id)\
+            .filter(TopicWordValue.topic_id == topic.id)\
+            .one()
+        maxes.append(tmax)
+        mins.append(tmin)
+        avgs.append(tavg)
+
+    p = figure(plot_height = 600, plot_width = 600,
+        x_axis_label = 'ID de Tópico',
+        y_axis_label = 'Valor',
+        tools="xpan, xwheel_zoom, box_zoom, reset, save",
+        x_range=[0,len(ids)]
+        )
+
+    p.line(ids, maxes, legend="Máximo", line_color="red")
+    p.line(ids, mins, legend="Mínimo", line_color="blue")
+    p.line(ids, avgs, legend="Promedio", line_color="green")
+    #p.multi_line(
+    #    xs = [ids, ids, ids],
+    #    ys = [maxes, mins, avgs],
+    #    line_color = ['#FF0000', '#0087FF', '#1D702D']
+    #    )
+    print(len(ids))
+    return p
+
+def twdis_topic(ts_id, twdis_id, topic_id):
+    """
+    """
+    ids = []
+    words = []
+    values = []
+
+    q = db.session.query(TopicWordValue)\
+        .filter(TopicWordValue.topicset_id == ts_id)\
+        .filter(TopicWordValue.twdis_id == twdis_id)\
+        .filter(TopicWordValue.topic_id == topic_id)\
+        .order_by(TopicWordValue.word_id)
+
+    for i, elem in enumerate(q):
+        ids.append(i)
+        words.append(Word.query.filter_by(id = elem.word_id).one().word_string)
+        values.append(elem.value)
+
+    panorama = figure(plot_height = 200, plot_width = 600,
+        x_axis_label = 'Palabras (ordenadas por ID)',
+        y_axis_label = 'Valor',
+        tools="xpan, xwheel_zoom, box_zoom, reset, save",
+        x_range=[-2,len(ids)]
+        )
+
+    panorama.line(ids, values)
+
+    data = {'id': ids, 'word': words, 'value': values}
+
+    full = figure(
+        y_range= words,
+        plot_width=600,
+        x_axis_label = "Valor",
+        y_axis_label ="Palabras (ordenadas por ID)",
+        tools="ypan, ywheel_zoom, box_zoom, reset, save",
+        tooltips=[("Palabra", "@word"), ("Valor", "@value"), ("ID", "@id")]
+        )
+
+    full.hbar(
+        source = data,
+        y = 'word',
+        right = 'value',
+        height=0.5
+        )
+
+    return panorama, full
--- topics_viz/routes_plots.py
+++ topics_viz/routes_plots.py
@@ -7,17 +7,57 @@ import topics_viz.plots as plotter
 @app.route("/ts<int:ts_id>/plots")
 def plots(ts_id):
     tset = db.session.query(TopicSet).filter(TopicSet.id == ts_id).one() # para que si no existe el topicset, suceda un error
-    return render_template('plots/plots_main.html', ts_id = tset.id)
+    twdis_list = db.session.query(TopicWordDistribution).filter(TopicWordDistribution.topicset_id == tset.id)
 
-@app.route("/ts<int:ts_id>/plots_topics_nwords")
+    return render_template('plots/plots_main.html', ts_id = tset.id, twdis_list = twdis_list)
+
+@app.route("/plot/ts<int:ts_id>/plots_topics_nwords")
 def plot_topics_nwords(ts_id):
     tset = db.session.query(TopicSet).filter(TopicSet.id == ts_id).one() # para que si no existe el topicset, suceda un error
     p = plotter.topicid_nwords_vbar(ts_id)
     p2 = plotter.topics_nwords_histogram(ts_id)
     script, div = components((p, p2))
-    print(div)
     return render_template('plots/plots_topics-nwords.html', ts_id = ts_id,
         script = script, div = div[0], div2 = div[1])
+
+@app.route("/plot/ts<int:ts_id>/plots_words_ntopics")
+def plot_words_ntopics(ts_id):
+    tset = db.session.query(TopicSet).filter(TopicSet.id == ts_id).one() # para que si no existe el topicset, suceda un error
+    p = plotter.wordid_ntopics_vbar(ts_id)
+    p2 = plotter.words_ntopics_histogram(ts_id)
+    script, div = components((p, p2))
+    return render_template('plots/plots_words-ntopics.html', ts_id = ts_id,
+        script = script, div = div[0], div2 = div[1])
+
+@app.route("/plot/ts<int:ts_id>/twdis<int:twdis_id>/summary")
+def plot_twdis_summary(ts_id, twdis_id):
+    tset = db.session.query(TopicSet).filter(TopicSet.id == ts_id).one() # para que si no existe el topicset, suceda un error
+    twdis = db.session.query(TopicWordDistribution)\
+        .filter(TopicWordDistribution.topicset_id == tset.id)\
+        .filter(TopicWordDistribution.id == twdis_id).one() # para que si no existe la twdis suceda un error
+
+    p = plotter.twdis_summary(ts_id, twdis_id)
+    script, div = components(p)
+    return render_template('plots/plots_twdis_summary.html', ts_id = ts_id,
+        twdis = twdis, script = script, div = div)
+
+@app.route("/plot/ts<int:ts_id>/twdis<int:twdis_id>/topic<int:topic_id>")
+def plot_twdis_topic(ts_id, twdis_id, topic_id):
+    tset = db.session.query(TopicSet).filter(TopicSet.id == ts_id).one() # para que si no existe el topicset, suceda un error
+
+    topic = db.session.query(Topic)\
+        .filter(Topic.topicset_id == ts_id)\
+        .filter(Topic.id == topic_id).one()
+
+    twdis = db.session.query(TopicWordDistribution)\
+        .filter(TopicWordDistribution.topicset_id == tset.id)\
+        .filter(TopicWordDistribution.id == twdis_id).one() # para que si no existe la twdis suceda un error
+
+
+    panorama, full = plotter.twdis_topic(ts_id, twdis_id, topic_id)
+    script, div = components((panorama, full))
+    return render_template('plots/plots_twdis_topic.html', ts_id = ts_id,
+        twdis = twdis, topic = topic, script = script, div = div[0], div2 = div[1])
 
 @app.route("/p")
 def p():
